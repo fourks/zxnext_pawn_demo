5bce0c0187995fa54c910ffe5a1ca1bd07a8b573
stefanbylund <stefan.bylund99@gmail.com>
2018-02-10 20:36:07 +0100

    Update run emulator batch files.

--- run_cspect.bat
+++ run_cspect.bat
@@ -1,10 +1,14 @@
 @echo off
 
-REM Add the path to CSpect.exe in your PATH environment variable (e.g. C:\cspect)
-REM or prepend the path directly in this batch file (e.g. C:\cspect\CSpect.exe).
+REM Add the path to CSpect.exe in your PATH environment variable (e.g. C:\cspect) or
+REM set CSPECT_HOME in this batch file by uncommenting and modifying the example below.
+
+for /f %%a in ('where CSpect.exe') do set CSPECT_HOME=%%~dpa
+
+REM set CSPECT_HOME=C:\cspect
 
 set DEMODIR=%~dp0
 
 @echo on
 
-CSpect.exe -w2 -zxnext -mmc=%DEMODIR% %DEMODIR%\pawndemo.sna
+%CSPECT_HOME%\CSpect.exe -w2 -zxnext -mmc=%DEMODIR% %DEMODIR%\pawndemo.sna
